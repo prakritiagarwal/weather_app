e81f028be84f3492b1576e28940d40ac6f37f04c
Prakriti <prakritiagarwal@outlook.com>
2022-04-09 17:37:55 -0400

    API call is working now

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 function getWeather() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);    
-    document.getElementById("myList").appendChild(li);    
-    document.getElementById("myInput").value = ""; 
+    let inputValue = document.getElementById("zipcode_input").value;
+    console.log(inputValue);
+
+    let apiKey = '025551c2ec00d510aa3cb30f1a56a741';
+
+    let apiUrl=`http://api.openweathermap.org/geo/1.0/zip?zip=${inputValue},US&appid=${apiKey}`;
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((citydataJson) => {
+        console.log(citydataJson);
+        
+        let city=citydataJson.name;
+        let lat=citydataJson.lat;
+        let lon=citydataJson.lon;
+        console.log(city);
+        console.log(lat);
+        console.log(lon);
+
+        let apiUrl2=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+        fetch(apiUrl2)
+        .then((response) => response.json())
+        .then((weatherdataJson) => {
+            console.log(weatherdataJson);
+            let current_temp=weatherdataJson.current.temp;
+            let current_condition=weatherdataJson.current.weather[0].description;
+            let humidity=weatherdataJson.current.humidity;
+            let tempMax = weatherdataJson.daily[0].temp.max;
+            let tempMin = weatherdataJson.daily[0].temp.min;  
+            const date = new Date().toLocaleDateString('en-us', { weekday:"short", month:"short", day:"numeric", hour:"numeric", minute:"numeric"})
+     
+            
+            document.querySelector("#City").innerText = city;
+            document.querySelector("#Temperature").innerText = current_temp;
+            document.querySelector("#Date").innerText = date;
+            document.querySelector("#Conditions").innerText = current_condition;
+             document.querySelector("#Humidity").innerText = humidity;
+             document.querySelector("#High").innerText=tempMax;
+             document.querySelector("#Low").innerText=tempMin;
+
+
+    });
+});
 }
